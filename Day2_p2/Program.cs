c95a197e043970dfcf5bce15c518193f5eeca548
Sean <sean.edwards@teachingpersonnel.com>
2023-12-04 14:39:44 +0000

    Day 2 part 2 completed

--- Day2_p2/Program.cs
+++ Day2_p2/Program.cs
@@ -1,1 +1,71 @@
-﻿
+﻿using System.Text.RegularExpressions;
+
+var runningTotal = 0;
+
+while (Console.ReadLine() is { } line)
+{
+    var gameInformation = line.Split(':');
+    var rounds = gameInformation.Last().Split(';', StringSplitOptions.TrimEntries);
+
+    int redCount = 0;
+    int greenCount = 0;
+    int blueCount = 0;
+    var roundCount = 0;
+
+    foreach (var round in rounds)
+    {
+        roundCount++;
+        var colourResults = round.Split(',', StringSplitOptions.TrimEntries);
+        foreach (var colourResult in colourResults)
+        {
+            var colorTypeAndCount = colourResult.Split(' ');
+            var colourAmount = GetFirstNumberFromString(colorTypeAndCount.First());
+            var colour = colorTypeAndCount.Last();
+
+            switch (colour)
+            {
+                case "red":
+                    if(colourAmount > redCount)
+                        redCount = colourAmount;
+                    break;
+                case "green":
+                    if (colourAmount > greenCount)
+                        greenCount = colourAmount;
+                    break;
+                case "blue":
+                    if (colourAmount > blueCount)
+                        blueCount = colourAmount;
+                    break;
+            }
+        }
+
+        if (rounds.Length == roundCount )
+        {
+            runningTotal += redCount * greenCount * blueCount;
+        }
+    }
+
+}
+
+Console.WriteLine(runningTotal);
+
+
+static int GetFirstNumberFromString(string input)
+{
+    Match match = MyRegex().Match(input);
+
+    if (match.Success)
+    {
+        return int.Parse(match.Value);
+    }
+    else
+    {
+        throw new Exception("NaN");
+    }
+}
+
+partial class Program
+{
+    [GeneratedRegex("\\d+")]
+    private static partial Regex MyRegex();
+}
--- Day2_p2/obj/Debug/net7.0/Day2_p2.AssemblyInfo.cs
+++ Day2_p2/obj/Debug/net7.0/Day2_p2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Day2_p2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9440bf8204741d7de2eb9053efbe0e1fc863a859")]
 [assembly: System.Reflection.AssemblyProductAttribute("Day2_p2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Day2_p2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
